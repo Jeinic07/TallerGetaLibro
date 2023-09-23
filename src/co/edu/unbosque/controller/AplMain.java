fadb477024e72beacde0f63f9917840405fd956c
MpGomezg <aome.mggm@gmail.com>
2023-09-23 18:12:48 -0500

    Aplmain, view and controller

---
+++ src/co/edu/unbosque/controller/AplMain.java
@@ -0,0 +1,15 @@
+package co.edu.unbosque.controller;
+
+import java.util.Scanner;
+
+import co.edu.unbosque.util.Edge;
+import co.edu.unbosque.util.Graph;
+import co.edu.unbosque.util.Vertex;
+
+public class AplMain {
+
+	public static void main(String[] args) {
+		Controller c = new Controller();
+			
+	}
+}
---
+++ src/co/edu/unbosque/controller/Controller.java
@@ -0,0 +1,46 @@
+package co.edu.unbosque.controller;
+import co.edu.unbosque.view.View;
+
+public class Controller {
+
+	
+	private View vi;
+
+	public Controller() {
+
+
+		start();
+	}
+
+	public void start() {
+		try {
+
+			while (true) {
+				
+
+				int opcion = vi.readInt();
+ 
+				vi.printJump("Welcome to CARELIBRO");
+				switch (opcion) {
+				case 1:
+				
+					break;
+				case 2:
+					
+					
+					break;
+				case 3:
+				
+					break;
+				default:
+					vi.printJump("Gracias por utilizar este programa");
+					System.exit(0);
+					break;
+				}
+			}
+		} catch (Exception e) {
+			vi.printJump("ERROR DEVISTE INGRESAR UN DATO MAL REINICIA EL PROGRAMA:(");
+		}
+	}
+
+}
