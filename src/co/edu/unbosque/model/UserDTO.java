45f58ade49fdebdf5fd594683a45c4432d6b9ff4
MpGomezg <aome.mggm@gmail.com>
2023-09-23 18:00:09 -0500

    Update UserDTO.java

--- src/co/edu/unbosque/model/UserDTO.java
+++ src/co/edu/unbosque/model/UserDTO.java
@@ -1,5 +1,44 @@
 package co.edu.unbosque.model;
 
-public class UserDTO {
+import co.edu.unbosque.util.MyLinkedList;
 
+public class UserDTO {
+	private String username;
+	private String password;
+	private MyLinkedList<UserDTO> friendList;
+	
+	public UserDTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public UserDTO(String username, String password, MyLinkedList<UserDTO> friendList) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.friendList = friendList;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public MyLinkedList<UserDTO> getFriendList() {
+		return friendList;
+	}
+
+	public void setFriendList(MyLinkedList<UserDTO> friendList) {
+		this.friendList = friendList;
+	}
 }
